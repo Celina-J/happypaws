2f3fdf911a856ee9d558c0ca724959c2f5c896b6
Celina <92083847+Celina-J@users.noreply.github.com>
2022-09-29 21:32:21 +0200

    navigationsbar
    börjat med uppsättning av navigationsbar, lite styling

--- themes/happypaws/functions.php
+++ themes/happypaws/functions.php
@@ -15,4 +15,20 @@ function google_fonts() {
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Montserrat&display=swap', false );
 }
 add_action( 'wp_enqueue_scripts', 'google_fonts' );
-?>
+
+
+//Navigationsbar
+
+function register_menus() { 
+    register_nav_menus(
+        array(
+            'right-main-menu' => 'Right Main Menu',
+            'left-main-menu' => 'Left Main Menu',
+            'footer-menu' => 'Footer Menu',
+        )
+    ); 
+}
+add_action( 'init', 'register_menus' );
+
+//$navbar_cart = wp_get_nav_menu_items('top-menu')[8]->title;
+
--- themes/happypaws/header.php
+++ themes/happypaws/header.php
@@ -10,4 +10,25 @@
 <body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
 
-<div>Här är header</div>
+  <div class="navigation-container">
+
+    <?php
+    wp_nav_menu(
+      array(
+        'theme_location' => 'left-main-menu'
+      )
+    );
+?>
+
+<div class="site-title-navbar">
+    <h1><a><?= get_bloginfo('name');?></a></h1>
+</div>
+
+<?php
+    wp_nav_menu(
+      array(
+        'theme_location' => 'right-main-menu'
+      )
+    );
+    ?>
+  </div>
