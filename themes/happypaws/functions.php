11cc3fd99252151334d11896597346ec9c29e6ae
Celina <92083847+Celina-J@users.noreply.github.com>
2022-10-03 12:55:33 +0200

    ed

--- themes/happypaws/functions.php
+++ themes/happypaws/functions.php
@@ -11,6 +11,7 @@ function add_scripts() {
     wp_enqueue_style( 'navbar-style', get_stylesheet_directory_uri() . '/styles/navbar.css', false );
     wp_enqueue_style( 'footer-style', get_stylesheet_directory_uri() . '/styles/footer.css', false );
     wp_enqueue_style( 'checkout-style', get_stylesheet_directory_uri() . '/styles/checkout.css', false );
+    wp_enqueue_style( 'cart-style', get_stylesheet_directory_uri() . '/styles/cart.css', false );
     wp_enqueue_script( 'navbar-script', get_template_directory_uri() . '/js/navbar.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'add_scripts' );
