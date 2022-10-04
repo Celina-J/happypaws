948381d73c6869a00b8ad2591b2606f2c9f72568
Celina <92083847+Celina-J@users.noreply.github.com>
2022-10-04 09:34:03 +0200

    byter branch

--- themes/happypaws/footer.php
+++ themes/happypaws/footer.php
@@ -6,32 +6,32 @@
             <div class="footer-menu-title"><strong>Navigate</strong></div>
             <?php
 
-            wp_nav_menu(
-                array(
-                    'theme_location' => 'navigate-footer-menu'
-                )
-            );
-            ?>
+wp_nav_menu(
+    array(
+        'theme_location' => 'navigate-footer-menu'
+        )
+    );
+    ?>
         </div>
-
-
+        
+        
         <div class="info-footer-menu">
             <div class="footer-menu-title"><strong>Information</strong></div>
             <?php
             wp_nav_menu(
                 array(
                     'theme_location' => 'info-footer-menu'
-                )
-            );
-            ?>
+                    )
+                );
+                ?>
         </div>
-
-
+        
+        
         <div class="site-title-navbar">
             <h1><a href="/"><?= get_bloginfo('name'); ?></a></h1>
         </div>
     </div>
-
+    
 </div>
 
 </body>
